50e130838dbdba261352e8997f51912be58ea5b4
jon5307 <jang3567027@gmail.com>
2022-12-28 22:54:46 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,11 +1,5 @@
-import sys
+import sys, math
 input = sys.stdin.readline
 
 a, b, v = map(int, input().split())
-daily = a - b
-v -= b
-day = v / daily
-if day.is_integer():
-    print(int(day))
-else:
-    print(int(day) + 1)
+print(math.ceil((v-b)/(a-b)))
