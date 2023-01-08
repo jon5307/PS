e5ed4e102f393d49d63ecfe84f48cca04262ca05
jon5307 <jang3567027@gmail.com>
2023-01-09 01:11:06 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 884 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -1,10 +1,11 @@
 def move(n,f,t,b):
     if n == 1:
-        return f,t
+        print(f,t)
     else:
-        return move(n-1,f,b,t) + move(1,f,t,b) + move(n-1,b,t,f)
+        move(n-1,f,b,t) 
+        move(1,f,t,b)
+        move(n-1,b,t,f)
 
-a = move(int(input()),1,3,2)
-print(len(a)//2)
-for i in range(0,len(a),2):
-    print(a[i],a[i+1])
+n = int(input())
+print(2**n-1)
+a = move(n,1,3,2)
