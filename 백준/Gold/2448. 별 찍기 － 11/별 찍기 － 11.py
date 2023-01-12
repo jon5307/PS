fbf82d4fce76135e2e00c9d29ecb483738f8f6e4
jon5307 <jang3567027@gmail.com>
2023-01-12 15:14:23 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 3352 ms, Memory: 388556 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
@@ -0,0 +1,29 @@
+from sys import stdin
+input = stdin.readline
+
+def star_map(star, size, x, y):
+    if size == 3:
+        star[y][x+2] = True
+        star[y+1][x+1] = True
+        star[y+1][x+3] = True
+        star[y+2][x] = True
+        star[y+2][x+1] = True
+        star[y+2][x+2] = True
+        star[y+2][x+3] = True
+        star[y+2][x+4] = True
+    else:
+        next = size//2
+        star_map(star,next,x+next,y)
+        star_map(star,next,x,y+next)
+        star_map(star,next,x+size,y+next)
+
+N = int(input())
+star = [[False for _ in range(2*N-1)] for _ in range(N)]
+star_map(star,N,0,0)
+for i in range(N):
+    for j in range(2*N-1):
+        if star[i][j]:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
