173fe4556e8cb48a34ba86def13e32943b559a0e
jon5307 <jang3567027@gmail.com>
2023-01-26 02:22:32 +0900

    [Silver I] Title: Z, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,19 +1,10 @@
-def z(n,r,c,start):
-    while n != 1:
-        if 2**(n-1) > c: # left side 
-            if 2**(n-1) <= r: # down side
-                r -= 2**(n-1)
-                start += 2**(n*2-1)
-        else: # right side
-            if 2**(n-1) > r: # up side
-                c -= 2**(n-1)
-                start += 2**((n-1)*2)
-            else: # down side
-                r -= 2**(n-1)
-                c -= 2**(n-1)
-                start += 3*2**((n-1)*2)
-        n -= 1
-    return start+2*r+c
-
-n,r,c = map(int,input().split())
-print(z(n,r,c,0))
+n,r,c=map(int,input().split())
+ans = 0
+for i in range(n-1,-1,-1):
+    ans <<= 1
+    if r & (1 << i):
+        ans += 1
+    ans <<= 1
+    if c & (1 << i):
+        ans += 1
+print(ans)
