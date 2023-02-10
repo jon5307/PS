9e904458bffd4326c9c294d72a5ad266ccf9ed4c
jon5307 <jang3567027@gmail.com>
2023-02-10 22:16:40 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,16 @@
+def two_by_n(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        dp = [0 for _ in range(n)]
+        dp[0] = 1
+        dp[1] = 2
+
+        for i in range(2,n):
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007
+
+        return dp[n-1]
+
+print(two_by_n(int(input())))
