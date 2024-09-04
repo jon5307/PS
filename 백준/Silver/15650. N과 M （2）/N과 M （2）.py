34b51266d267aac013ce97407202a55727ee030d
jon5307 <jang3567027@gmail.com>
2024-09-04 15:08:33 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,17 @@
+def bfs(N, M, l):
+    if len(l) == M:
+        print(*l, sep=" ")
+        return
+    if len(l) == 0:
+        for i in range(1, N + 1):
+            bfs(N, M, [i])
+        return
+    if l[-1] > N:
+        return
+    for i in range(l[-1] + 1, N + 1):
+        bfs(N, M, l + [i])
+
+
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    bfs(N, M, [])
