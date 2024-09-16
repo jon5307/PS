873c0db02f85031b65f07e16bea429a883bbed64
jon5307 <jang3567027@gmail.com>
2024-09-16 17:14:57 +0900

    [Gold IV] Title: N-Queen, Time: 15512 ms, Memory: 146988 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -6,20 +6,39 @@ n = int(input())
 
 
 def dfs(col: int, filled: list, n: int):
-    if col == n:
-        return 1
+    sum = 0
+    if col == n - 1:
+        for i in range(n):
+            use = True
+            for j in range(len(filled)):
+                if (
+                    i == filled[j]
+                    or i - filled[j] == j - col
+                    or i - filled[j] == col - j
+                ):
+                    use = False
+                    break
+            if use:
+                sum += 1
+        return sum
+    elif col == 0:
+        for i in range(n):
+            sum += dfs(col + 1, filled + [i], n)
+        return sum
+    else:
+        for i in range(n):
+            use = True
+            for j in range(len(filled)):
+                if (
+                    i == filled[j]
+                    or i - filled[j] == j - col
+                    or i - filled[j] == col - j
+                ):
+                    use = False
+                    break
+            if use:
+                sum += dfs(col + 1, filled + [i], n)
+        return sum
 
-    count = 0
-    for i in range(n):
-        valid = True
-        for j in range(col):
-            if i == filled[j] or abs(i - filled[j]) == abs(col - j):
-                valid = False
-                break
-        if valid:
-            filled[col] = i
-            count += dfs(col + 1, filled, n)
-    return count
 
-
-print(dfs(0, [-1 for _ in range(n)], n))
+print(dfs(0, [], n))
