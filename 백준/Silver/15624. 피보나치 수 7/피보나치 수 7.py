d9a8a9ac999c3567b901b70c511ca76aaa18cccd
jon5307 <jang3567027@gmail.com>
2024-09-29 16:59:15 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 32 ms, Memory: 31120 KB, Score: 201 point -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -0,0 +1,27 @@
+from sys import stdin
+
+input = stdin.readline
+divider = 1000000007
+
+
+def divide_conquer(n):
+    if n == 1:
+        return (1, 1, 0)
+    a0, a1, a2 = divide_conquer(n // 2)
+    n0 = ((a0**2) % divider + (a1**2) % divider) % divider
+    n1 = ((a0 * a1) % divider + (a1 * a2) % divider) % divider
+    n2 = ((a1**2) % divider + (a2**2) % divider) % divider
+    if n % 2 == 0:
+        return (n0, n1, n2)
+    else:
+        return ((n0 + n1) % divider, n0, n1)
+
+
+n = int(input())
+if n == 0:
+    print(0)
+elif n == 1:
+    print(1)
+else:
+    ans, _, _ = divide_conquer(n - 1)
+    print(ans)
