84846f3db12ae575a667315c926334603cf15995
jon5307 <jang3567027@gmail.com>
2024-10-10 16:09:32 +0900

    [Silver II] Title: N과 M (9), Time: 100 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,31 @@
+from sys import stdin
+
+input = stdin.readline
+
+n, m = map(int, input().rstrip().split())
+
+count = {}
+nums = []
+for num in map(int, input().rstrip().split()):
+    if num in count:
+        count[num] += 1
+    else:
+        count[num] = 1
+    if not num in nums:
+        nums.append(num)
+
+nums.sort()
+
+
+def dfs(depth, seq):
+    if depth > m:
+        print(*seq)
+    else:
+        for num in nums:
+            if num in count and count[num] != 0:
+                count[num] -= 1
+                dfs(depth + 1, seq + [num])
+                count[num] += 1
+
+
+dfs(1, [])
